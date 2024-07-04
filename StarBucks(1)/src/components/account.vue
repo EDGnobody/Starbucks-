cd61e35ac131e4515bbc7525af82bb9834ed6906
XXXXXfish <1741671878@qq.com>
2024-07-04 17:04:56 +0800

    account

--- StarBucks(1)/src/components/account.vue
+++ StarBucks(1)/src/components/account.vue
@@ -26,7 +26,18 @@
   </el-card>
   <!--  -->
   <el-card class="card" style="margin-top: 10px;">
-    <img src="../images/image.png" class="equilt" style="width: 90%"></img>
+    <div> 
+      <h2>星享日常所爱</h2>
+	      <div>	
+		      <img class="image" src="../images/account-1.png" alt="image">
+		      <p>
+            一份指定中杯手工调制饮品/星巴克\浓/系列饮品/烘焙食品/酸奶/水果沙拉/三明治
+          </p>
+	      </div>
+    </div>
+<!--     
+    <img src="../images/account-01.png" class="equilt" style="width: 90%"></img>
+    <img src="../images/account-02.png" class="equilt" style="width: 90%"></img> -->
   </el-card>
   <!-- 消费记录 -->
   <el-card class="card" style="width: 100%;margin-top: 10px;">
@@ -105,5 +116,10 @@ function change(){
     align-items: center;
     padding: 10px;
     /* style="text-align: right; margin: 0" */
+}
+.image{
+    width: 200px;
+    height: auto;
+    float: right;
 }
 </style>
